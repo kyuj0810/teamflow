a44abab02c5e8bdb072b56b5fe8bff9f9cf76d41
jin <kyuj0810@gmail.com>
2026-01-14 18:47:03 +0900

    id uuid 형식 validation 검증 추가

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
 import { createUserSchema } from '../dto/create-user.dto';
-import { createUserService, updateUserService } from '../services/user.service';
 import { updateUserSchema } from 'src/dto/update-user.dto';
+import { userIdParamSchema } from 'src/dto/user-params.dto';
+import { createUserService, updateUserService } from '../services/user.service';
 
 export const createUser = async (req: Request, res: Response) => {
   const body = createUserSchema.parse(req.body);
@@ -11,10 +12,13 @@ export const createUser = async (req: Request, res: Response) => {
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-  const id = req.params.id;
+  // 1. URL 검증
+  const { id } = userIdParamSchema.parse(req.params);
 
+  // 2. Body 검증
   const body = updateUserSchema.parse(req.body);
 
+  // 3. 서비스 호출
   const updatedUser = await updateUserService(id, body);
 
   res.json(updatedUser);
